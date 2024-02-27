1a1c79b8b2a1337777ce44fe674c192b76a9551a
ALUNO <ALUNO@LABHPC06>
2024-02-27 15:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anonasc, idade;
+            Console.WriteLine("Digite o ano que você nasceu:");
+            anonasc = int.Parse(Console.ReadLine());
+            idade = 2024 - anonasc;
+            if (idade >= 16 && idade >= 18)
+            {
+                Console.WriteLine("Você já tem idade para votar e para conseguir a C.N.H.");
+            }
+            else if (idade >= 16)
+            {
+                Console.WriteLine("Você já tem idade para votar.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
